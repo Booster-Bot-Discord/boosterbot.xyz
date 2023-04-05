b00a3779a2d33655278613352536dea13a5f911b
Ankit Malik <ankit.malik@zomato.com>
2023-04-06 03:17:35 +0530

    dashboard disclamer

--- src/pages/singular/Landing.jsx
+++ src/pages/singular/Landing.jsx
@@ -5,7 +5,7 @@ import "../../assets/styles/pages/singular/landing.scss";
 const cardData = [
 //   { name: "Commands", description: "See all of the features Booster Bot has to offer your server!", link: "/commands" },
   { name: "Support", description: "FAQ page not enough to help? Head over to our support server!", link: "https://discord.gg/8kdx63YsDf" },
-  { name: "Dashboard", description: "Setup the bot with ease of a web dashboard and premium support!", link: "https://dashboard.boosterbot.xyz/" },
+  { name: "Dashboard", description: "Setup the bot with ease of dashboard (work in progress, docs & commands are recommended)", link: "https://dashboard.boosterbot.xyz/" },
   { name: "Docs", description: "Checkout the bot documentation to see the deatures Booster Bot offers.", link: "https://docs.boosterbot.xyz/" },
   { name: "Premium", description: "You can vote to get premium OR check this link and unlock limits on perks.", link: "https://dashboard.boosterbot.xyz/premium" },
 ];
